b954323efd1cf7099830a3fdd5336845b434c69a
Teodora Vermesan <vermesanteodora@gmail.com>
2023-12-27 22:36:28 +0200

    refactoring  functions

--- index.js
+++ index.js
@@ -12,39 +12,41 @@ function hide(id) {
 function show(id) {
   $(`#${id}`).style.display = "block";
 }
-
+//better to have function to do specific things
 function showPage(id) {
   hide(activePage);
+  //querySelector
   $(`#top-menu-bar a[data-page="${activePage}"]`).classList.remove("active");
   show(id);
   $(`#top-menu-bar a[data-page="${id}"]`).classList.add("active");
   activePage = id;
 }
 
-showPage(activePage);
-
-$("#top-menu-bar").addEventListener("click", function (e) {
-  var id = e.target.dataset.page;
-  console.info("Click on", id);
-  if (id) {
-    showPage(id);
-  }
-});
-//querySelector
-//use array.map to create skills list
-fetch("skills.json")
-  .then(function (response) {
-    //transform the response into jason
-    console.info("done?");
-    return response.json();
-  })
-  //the data is transform then the function is transformed
-  .then(function (skills) {
-    //call the function to print skills
-    printSkills(skills);
+function initEvents() {
+  //give me the element, add the event listener, the element is clicked and then we have a function that is executed every time at click
+  $("#top-menu-bar").addEventListener("click", function (e) {
+    var id = e.target.dataset.page;
+    console.info("Click on", id);
+    if (id) {
+      showPage(id);
+    }
   });
+}
+//use array.map to create skills list
+function loadSkills() {
+  fetch("skills.json")
+    .then(function (response) {
+      //transform the response into json
+      console.info("done?");
+      return response.json();
+    })
+    //the data is transform then the function is transformed
+    .then(function (skills) {
+      //call the function to print skills
+      printSkills(skills);
+    });
+}
 
-//better to gave function to do specific things
 //get the skills from outside and the print them
 function printSkills(skills) {
   skills = sortSkillsByEndorcements(skills);
@@ -68,3 +70,9 @@ function sortSkillsByName(skills) {
     return a.name.localeCompare(b.name);
   });
 }
+showPage(activePage);
+initEvents();
+loadSkills();
+
+// The fetch() method starts the process of fetching a resource from a server.
+// The fetch() method returns a Promise that resolves to a Response object.

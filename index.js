d34e81d26d0000556f1f2d48200262563b5d952a
Teodora Vermesan <vermesanteodora@gmail.com>
2024-01-12 18:10:23 +0200

    add sticky hesder + refactor

--- index.js
+++ index.js
@@ -128,3 +128,20 @@ loadEducation();
 loadExperience();
 loadLanguages();
 loadSkills();
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var header = document.getElementById("header");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+onscroll();
